07943d6c0e33216168161d86a13ece6a9abb46b1
Tony Lofreso <anthonylofreso@westfieldgrp.com>
2021-12-01 12:34:22 -0500

    solved puzzle two

--- 01/depth.py
+++ 01/depth.py
@@ -3,11 +3,24 @@ from rich import print
 with open('input', 'r') as f:
     data = [int(line) for line in f.read().splitlines()]
 
-count = 0
-
+# Puzzle 1
+count1 = 0
 for index, depth in enumerate(data):
     if depth > data[index - 1]:
-        count += 1
+        count1 += 1
 
-print(f'Puzzle one: {count}')
-print(f'Puzzle two: ---')
+# Puzzle 2
+count2 = 0
+groups = []
+for index, depth in enumerate(data):
+    if index <= len(data) - 3:
+        group = sum([depth, data[index+1], data[index+2]])
+        groups.append(group)
+
+for index, group in enumerate(groups):
+    if group > groups[index - 1]:
+        count2 +=1
+
+
+print(f'Puzzle one: {count1}')
+print(f'Puzzle two: {count2}')
