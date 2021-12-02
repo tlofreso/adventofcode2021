70a3486247fa17be6f5a01e1102c838137e33dd8
Tony Lofreso <tonylofresojr@gmail.com>
2021-12-02 00:38:45 -0500

    solved day 2

---
+++ 02/position.py
@@ -0,0 +1,49 @@
+from rich import print
+
+with open('input', 'r') as f:
+    data = f.read().splitlines()
+
+
+# Puzzle 1
+positions = []
+for e in data:
+    position = {e.split()[0]:e.split()[1]}
+    positions.append(position)
+
+horizontal = 0
+depth = 0
+for position in positions:
+    for k, v in position.items():
+        if k == 'forward':
+            horizontal += int(v)
+        if k == 'up':
+            depth -= int(v)
+        if k == 'down':
+            depth += int(v)
+
+final1 = horizontal * depth
+
+# Puzzle 2
+positions = []
+for e in data:
+    position = {e.split()[0]:e.split()[1]}
+    positions.append(position)
+
+horizontal = 0
+depth = 0
+aim = 0
+for position in positions:
+    for k, v in position.items():
+        if k == 'forward':
+            horizontal += int(v)
+            depth_delta = aim * int(v)
+            depth += depth_delta
+        if k == 'up':
+            aim -= int(v)
+        if k == 'down':
+            aim += int(v)
+
+final2 = horizontal * depth
+
+print(f'Puzzle one: {final1}')
+print(f'Puzzle two: {final2}')
